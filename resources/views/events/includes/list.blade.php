d2cfc9b9003dd0d26ea138ec530dadd7ab7db95b
rspt <julien@rspt.ch>
2020-11-11 18:29:44 +0100

    Majuscule pour les dates de l'agenda écris en toutes lettres

--- resources/views/events/includes/list.blade.php
+++ resources/views/events/includes/list.blade.php
@@ -1,14 +1,14 @@
 @if ($date->isYesterday())
     <h2 class="text-primary mb-0">Hier</h2>
-    <p class="text-muted">{{ $date->isoFormat('dddd D MMMM Y') }}</p>
+    <p class="text-muted">{{ ucfirst($date->isoFormat('dddd D MMMM Y')) }}</p>
 @elseif ($date->isSameDay())
     <h2 class="text-primary mb-0">Aujourd'hui</h2>
-    <p class="text-muted">{{ $date->isoFormat('dddd D MMMM Y') }}</p>
+    <p class="text-muted">{{ ucfirst($date->isoFormat('dddd D MMMM Y')) }}</p>
 @elseif ($date->isTomorrow())
     <h2 class="text-primary mb-0">Demain</h2>
-    <p class="text-muted">{{ $date->isoFormat('dddd D MMMM Y') }}</p>
+    <p class="text-muted">{{ ucfirst($date->isoFormat('dddd D MMMM Y')) }}</p>
 @else
-    <h2 class="text-primary mb-3">{{ $date->isoFormat('dddd D MMMM Y') }}</h2>
+    <h2 class="text-primary mb-3">{{ ucfirst($date->isoFormat('dddd D MMMM Y')) }}</h2>
 @endif
 
 @forelse ($events as $event)
