4125cb418b17edc88fbc917b934002832c9775eb
rspt <julien@rspt.ch>
2020-12-13 13:40:51 +0100

    Alignement du design de la page d'un espace avec celui de la page d'un évènement

--- resources/views/venues/show.blade.php
+++ resources/views/venues/show.blade.php
@@ -2,27 +2,32 @@
 
 @section('content')
 <div class="container">
-    @if ($venue->logo)
-        <div class="text-center mb-5">
-            <a href="{{ Storage::url($venue->logo) }}">
-                <img src="{{ $venue->logo962 }}" alt="Logo {{ $venue->name }}" class="img-fluid">
-            </a>
-        </div>
-    @endif
+    <h1 class="text-primary">{{ $venue->name }}</h1>
 
-    <h1>{{ $venue->name }}</h1>
+    <p>
+        @include('venues.includes.contact')
+    </p>
+
+    <div class="row mt-5">
+        @if ($venue->logo)
+            <div class="col-md mb-4">
+                <a href="{{ Storage::url($venue->logo) }}">
+                    <img src="{{ $venue->logo962 }}" alt="Logo {{ $venue->name }}" class="img-fluid">
+                </a>
+            </div>
+        @endif
+        <div class="col-md">
+            @if ($venue->description)
+                <p>
+                    {!! nl2br(e($venue->description)) !!}
+                </p>
+            @endif
+        </div>
+    </div>
+
     @can('update', $venue)
         <a href="{{ route('venues.edit', $venue) }}" class="btn btn-primary mb-3">Modifier l'espace</a>
     @endcan
-
-    <section class="mt-5">
-        @if ($venue->description)
-            <p>
-                {!! nl2br(e($venue->description)) !!}
-            </p>
-        @endif
-        @include('venues.includes.contact')
-    </section>
 
     <section class="mt-5">
         <h2>Évènements à venir</h2>
